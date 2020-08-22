75372baa202c77713f382b8d95f623d97103521c
Ethan Wang <ethanwang422@gmail.com>
2020-08-22 18:09:28 -0400

    change app to class

--- hackthe6ix/src/App.js
+++ hackthe6ix/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from "react";
 import './App.css';
 import Homepage from './components/Homepage.js';
 import './styles/style.css';
 
 
-function App() {
-  return (
-      <div className='App'>
-          
+class App extends Component {
+  render() {
+    return (
+        <div className='App'>
           <Homepage colour = "blue"/>
         </div>
-      );
-    }
+        );
+      }
+  }
 
 export default App;
