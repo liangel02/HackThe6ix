4d1f51fe66d539f546c591b5a253071c61c18362
Ethan Wang <ethanwang422@gmail.com>
2020-08-22 22:03:44 -0400

    Spotify API connected
    displays the song that the user is currently listening to

--- hackthe6ix/src/App.js
+++ hackthe6ix/src/App.js
@@ -3,15 +3,64 @@ import './App.css';
 import Homepage from './components/Homepage.js';
 import './styles/style.css';
 
+import SpotifyWebApi from 'spotify-web-api-js';
+const spotifyApi = new SpotifyWebApi();
+
 
 class App extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: {
+        name: 'unknown',
+        image: ''
+      }
+    }
+  }
+  
+  getHashParams = () => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying = () => {
+    spotifyApi.getMyCurrentPlaybackState()
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            image: response.item.album.images[0].url
+          }
+        })
+      })
+  }
   render() {
     return (
         <div className='App'>
           <Homepage colour = "blue"/>
+          
+          <div> Now playing {this.state.nowPlaying.name} </div>
+          <img src={this.state.nowPlaying.image} />
+          <button onClick={this.getNowPlaying}>
+            Check now playing!!
+          </button>
         </div>
         );
-      }
+    }
   }
 
 export default App;
